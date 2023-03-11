da78d48ca07c47289fcbb91dbebad8f99343df0f
Damien PORTÉ <damien.porte@soprasteria.com>
2023-03-11 23:44:52 +0100

    Test redaction.

---
+++ DomainDrivenDesign.Application.Tests/MessageServiceTest.cs
@@ -0,0 +1,32 @@
+namespace DomainDrivenDesign.Application.Tests;
+
+public class MessageServiceTest
+{
+    [Theory, AutoData]
+    public void Given_a_message_When_I_create_it_Then_it_is_persisted_with_its_creation_date_as_brouillon(MessageCreateDto messageCreateDto, DateTime now)
+    {
+        // Arrange
+        Mock<IDateTimeProvider> dateTimeProviderMock = new();
+        dateTimeProviderMock.Setup(x => x.Now()).Returns(now);
+        IDateTimeProvider dateTimeProvider = dateTimeProviderMock.Object;
+
+        Mock<IMessageRepository> messageRepositoryMock = new();
+        messageRepositoryMock.Setup(x => x.Insert(It.IsAny<Message>()));
+        IMessageRepository messageRepository = messageRepositoryMock.Object;
+
+        // Act
+        IMessageService service = new MessageService(dateTimeProvider, messageRepository);
+        service.Create(messageCreateDto);
+
+        // Assert
+        dateTimeProviderMock.Verify(p => p.Now(), Times.Once);
+        messageRepositoryMock.Verify(
+                r => r.Insert(It.Is<Message>(
+                    m =>
+                        m.Titre == messageCreateDto.Titre &&
+                        m.Description == messageCreateDto.Description &&
+                        m.Tags.SequenceEqual(messageCreateDto.Tags) &&
+                        m.Etat == EtatMessage.Brouillon)),
+                Times.Once);
+    }
+}
--- DomainDrivenDesign.Application.Tests/Usings.cs
+++ DomainDrivenDesign.Application.Tests/Usings.cs
@@ -2,3 +2,6 @@ global using AutoFixture;
 global using DomainDrivenDesign.Domain.Entities.MessageAggregate;
 global using FluentAssertions;
 global using Xunit;
+global using AutoFixture.Xunit2;
+global using Moq;
+global using DomainDrivenDesign.Application.Dtos;
