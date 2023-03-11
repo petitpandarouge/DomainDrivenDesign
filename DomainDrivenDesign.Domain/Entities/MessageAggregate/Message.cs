46983638e034ab0d8dab5d58964b54df52413ace
Damien PORTÉ <damien.porte@soprasteria.com>
2023-03-11 23:00:59 +0100

    Green

--- DomainDrivenDesign.Domain/Entities/MessageAggregate/Message.cs
+++ DomainDrivenDesign.Domain/Entities/MessageAggregate/Message.cs
@@ -5,11 +5,13 @@ public class Message
     public string Titre { get; }
     public string Description { get; }
     public IReadOnlyCollection<string> Tags { get; }
+    public DateTime CreationDate { get; }
 
-    public Message(string titre, string description, IEnumerable<string> tags)
+    public Message(string titre, string description, IEnumerable<string> tags, DateTime now)
     {
         Titre = titre;
         Description = description;
         Tags = tags.ToList().AsReadOnly();
+        CreationDate = now;
     }
 }
