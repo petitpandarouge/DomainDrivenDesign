3ab115539fdc634ba9a049d1a8df929a92729d41
Damien PORTÉ <damien.porte@soprasteria.com>
2023-03-12 15:03:09 +0100

    Red

---
+++ DomainDrivenDesign.Domain/Entities/MessageAggregate/Titre.cs
@@ -0,0 +1,11 @@
+﻿namespace DomainDrivenDesign.Domain.Entities.MessageAggregate;
+
+public class Titre
+{
+    private readonly string _titre;
+
+    public Titre(string titre)
+    {
+        _titre = titre;
+    }
+}
