817a9c075cc34e96c945a0e3b85cdcefaf8f43a3
Damien PORTÉ <damien.porte@soprasteria.com>
2023-03-12 18:31:46 +0100

    Test redaction.

---
+++ DomainDrivenDesign.Application.Tests/Factories/MessageFactory.cs
@@ -0,0 +1,18 @@
+﻿using AutoFixture;
+
+namespace DomainDrivenDesign.Application.Tests.Factories;
+
+internal static class MessageFactory
+{
+    public static Message CreateBrouillon(string titre, string description, IEnumerable<string> tags, DateTime now)
+    {
+        return new Message(titre, description, tags, now);
+    }
+
+    public static Message CreateMessagePublie(string titre, string description, IEnumerable<string> tags, DateTime now)
+    {
+        var message = CreateBrouillon(titre, description, tags, now);
+        message.Valider();
+        return message;
+    }
+}
--- DomainDrivenDesign.Application.Tests/MessageServiceTest.cs
+++ DomainDrivenDesign.Application.Tests/MessageServiceTest.cs
@@ -1,9 +1,12 @@
+using DomainDrivenDesign.Application.Tests.Factories;
+
 namespace DomainDrivenDesign.Application.Tests;
 
 public class MessageServiceTest
 {
     [Theory, AutoData]
-    public void Given_a_message_When_I_create_it_Then_it_is_persisted_with_its_creation_date_as_brouillon(MessageCreateDto messageCreateDto, DateTime now)
+    public void When_I_create_a_message_Then_it_is_persisted_with_its_creation_date_as_brouillon
+        (MessageCreateDto messageCreateDto, DateTime now)
     {
         // Arrange
         Mock<IDateTimeProvider> dateTimeProviderMock = new();
@@ -14,8 +17,9 @@ public class MessageServiceTest
         messageRepositoryMock.Setup(x => x.Insert(It.IsAny<Message>()));
         IMessageRepository messageRepository = messageRepositoryMock.Object;
 
-        // Act
         IMessageService service = new MessageService(dateTimeProvider, messageRepository);
+
+        // Act
         service.Create(messageCreateDto);
 
         // Assert
@@ -28,6 +32,38 @@ public class MessageServiceTest
                         m.Tags.SequenceEqual(messageCreateDto.Tags) &&
                         m.CreationDate == now &&
                         m.Etat == Etat.Brouillon)),
+                Times.Once);
+    }
+
+    [Theory, AutoData]
+    public void Given_a_message_brouillon_When_I_valide_Then_the_message_is_publie
+        (string titre, string description, IEnumerable<string> tags, DateTime now)
+    {
+        // Arrange
+        Mock<IDateTimeProvider> dateTimeProviderMock = new();
+        IDateTimeProvider dateTimeProvider = dateTimeProviderMock.Object;
+
+        var message = MessageFactory.CreateBrouillon(titre, description, tags, now);
+
+        Mock<IMessageRepository> messageRepositoryMock = new();
+        messageRepositoryMock.Setup(x => x.Get(message.Id)).Returns(message);
+        IMessageRepository messageRepository = messageRepositoryMock.Object;
+
+        IMessageService service = new MessageService(dateTimeProvider, messageRepository);
+
+        // Act
+        service.Valider(message.Id);
+
+        // Assert
+        messageRepositoryMock.Verify(r => r.Get(message.Id), Times.Once);
+        messageRepositoryMock.Verify(
+                r => r.Update(It.Is<Message>(
+                    m =>
+                        m.Titre == message.Titre &&
+                        m.Description == message.Description &&
+                        m.Tags.SequenceEqual(message.Tags) &&
+                        m.CreationDate == now &&
+                        m.Etat == Etat.Publie)),
                 Times.Once);
     }
 }
