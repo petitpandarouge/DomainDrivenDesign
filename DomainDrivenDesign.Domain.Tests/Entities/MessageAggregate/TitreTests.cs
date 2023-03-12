717d3cb9472eb7fa5d9b643d135def378a127adb
Damien PORTÉ <damien.porte@soprasteria.com>
2023-03-12 15:01:48 +0100

    Test redaction.

---
+++ DomainDrivenDesign.Domain.Tests/Entities/MessageAggregate/TitreTests.cs
@@ -0,0 +1,21 @@
+﻿namespace DomainDrivenDesign.Domain.Tests.Entities.MessageAggregate;
+
+public class TitreTests
+{
+    [Fact]
+    public void Given_a_null_titre_When_I_create_it_Then_it_should_throw_an_exception()
+    {
+        // Arrange
+        string? titre = null;
+
+        // Act
+#pragma warning disable CS8604 // Possible null reference argument.
+        var act = () => new Titre(titre);
+#pragma warning restore CS8604 // Possible null reference argument.
+
+        // Assert
+        act
+            .Should()
+            .ThrowExactly<ArgumentNullException>();
+    }
+}
