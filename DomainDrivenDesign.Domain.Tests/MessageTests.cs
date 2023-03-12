ec184e9ee56f22fbb91ce0013e3dc9c428207d31
Damien PORTÉ <damien.porte@soprasteria.com>
2023-03-12 01:04:35 +0100

    Refactor

--- DomainDrivenDesign.Domain.Tests/MessageTests.cs
+++ DomainDrivenDesign.Domain.Tests/MessageTests.cs
@@ -2,16 +2,12 @@ namespace DomainDrivenDesign.Domain.Tests;
 
 public class MessageTests
 {
-    [Fact]
-    public void When_I_create_a_message_Then_it_should_be_correctly_initialized()
+    [Theory, AutoData]
+    public void When_I_create_a_message_Then_it_should_be_correctly_initialized
+        (string titre, string description, IEnumerable<string> tags, DateTime now)
     {
         // Arrange
-        Fixture fixture = new();
-        string titre = fixture.Create<string>();
-        string description = fixture.Create<string>();
-        IEnumerable<string> tags = fixture.CreateMany<string>();
-        DateTime now = fixture.Create<DateTime>();
-        Etat etat = Etat.Brouillon;
+        // Nothing to do.
 
         // Act
         Message message = new(titre, description, tags, now);
@@ -21,7 +17,7 @@ public class MessageTests
         message.Description.Should().Be(description);
         message.Tags.Should().Equal(tags);
         message.CreationDate.Should().Be(now);
-        message.Etat.Should().Be(etat);
+        message.Etat.Should().Be(Etat.Brouillon);
     }
 
     [Theory, AutoData]
